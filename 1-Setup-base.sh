effef3514083db63caf65d405a1e5158851bb7c2
Carlo <carlocentos@gmail.com>
2017-12-25 22:40:34 +0000

    Update 1-Setup-base.sh

--- 1-Setup-base.sh
+++ 1-Setup-base.sh
@@ -4,7 +4,6 @@ sudo apt-get install -y nano
 sudo apt-get install -y git
 sudo apt-get install -y nautilus-open-terminal
 nautilus -q
-nautilus
 mkdir ~ubuntu/jetsonHacksStuffs
 cd ~ubuntu/jetsonHacksStuffs/
 git clone https://github.com/jetsonhacks/postFlash.git
